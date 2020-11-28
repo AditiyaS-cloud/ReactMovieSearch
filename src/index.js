082d090b70acdd76f5beeab0414be5d6057e50c3
AditiyaS-cloud <71305712+AditiyaS-cloud@users.noreply.github.com>
2020-11-28 19:45:00 +0530

    Integrated Fetch Api to show movie data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import SearchMovies from './searchMovie';
 class Main extends React.Component {
   render() {
     return (
       <div className="container">
         <h1 className="title">React Movie Search</h1>
+        <SearchMovies/>
       </div>
 
     );
